c261804d878a38fac94834f012348cdfdeb7c734
dlu <odon5ht@gmail.com>
2023-06-09 13:41:25 +0200

    update

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: dlu <dlu@student.42berlin.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/03 16:55:06 by dlu               #+#    #+#             */
-/*   Updated: 2023/06/09 12:13:20 by dlu              ###   ########.fr       */
+/*   Updated: 2023/06/09 13:41:23 by dlu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,13 +35,17 @@ int	main(int ac, char **av)
 		ft_perror_exit(ERR_ARG, NULL);
 	game_init(&game);
 	ft_map_parser(av[1], &game);
-	game.mlx = mlx_init(256, 256, "SO_LONG", true);
+	game.mlx = mlx_init(416, 160, "SO_LONG", false);
 	if (!game.mlx)
 		ft_perror_exit((char *) mlx_strerror(mlx_errno), &game);
+	
+	mlx_texture_t	*texture = mlx_load_png("./assets/wall.png");
+	mlx_image_t		*img = mlx_texture_to_image(game.mlx, texture);
+	mlx_resize_image(img, 32, 32);
+	mlx_image_to_window(game.mlx, img, 50, 0);
+	
 	mlx_key_hook(game.mlx, &ft_key_hook, &game);
 	mlx_close_hook(game.mlx, &ft_close_hook, &game);
 	mlx_loop(game.mlx);
 	return (EXIT_SUCCESS);
 }
-
-// only after validing the map will run mlx.
--- srcs/mlx_hook.c
+++ srcs/mlx_hook.c
@@ -6,7 +6,7 @@
 /*   By: dlu <dlu@student.42berlin.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/09 11:40:52 by dlu               #+#    #+#             */
-/*   Updated: 2023/06/09 12:22:36 by dlu              ###   ########.fr       */
+/*   Updated: 2023/06/09 13:33:50 by dlu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ void	ft_key_hook(mlx_key_data_t keydata, void *param)
 	t_game	*game;
 
 	game = param;
-	//if (mlx_is_key_down(game->mlx, MLX_KEY_ESCAPE))
-	//	mlx_close_window(game->mlx);
 	if (keydata.key == MLX_KEY_ESCAPE && keydata.action == MLX_PRESS)
 	{
 		ft_free_map(game->map);
